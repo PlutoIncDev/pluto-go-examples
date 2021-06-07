06a803ab2bb3f1064d6a12445ebdc1bc3d0967a1
Calum Webb <calumpeterwebb@icloud.com>
2021-06-06 21:39:22 -0700

    updated main.go

--- services/users/main.go
+++ services/users/main.go
@@ -8,13 +8,20 @@ import (
 func main() {
 	client := pluto.NewClient("test")
 
+	// Create Providers
 	httpProvider := http.NewProvider("8080")
 
-	httpProvider.RegisterEndpoint("GET", "/health-check", func() {
+	httpProvider.RegisterEndpoint(http.GetMethod, "/health-check", func() {
 
 	})
 
-	// Register the providers
+	// TODO; think about how to add a middleware which could authenticate the user
+	// TODO; think about how to add the :userID
+	httpProvider.RegisterEndpoint(http.GetMethod, "/user/:userID", func() {
+
+	})
+
+	// Register the Providers
 	client.RegisterProvider(httpProvider)
 
 	// Start the service
