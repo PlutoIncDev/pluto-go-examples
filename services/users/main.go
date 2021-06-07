7c82e947a62a43d374b2f69b62bc67f05d7006b1
Calum Webb <calumpeterwebb@icloud.com>
2021-06-07 02:01:39 -0700

    added user example

--- services/users/main.go
+++ services/users/main.go
@@ -1,29 +1,47 @@
 package main
 
 import (
+	"log"
 	"pluto/pkgs/pluto"
 	"pluto/pkgs/providers/http"
 )
 
-func main() {
+func handleHealthCheck(ctx *http.Context) {
+	log.Println("health check")
+
+	ctx.HTTP.JSON(200, http.H{"success": "true"})
+}
+
+func handleGetUserById(ctx *http.Context) {
+	log.Println("get user by id")
+
+	ctx.HTTP.JSON(200, http.H{"user": http.H{"id": "123", "email": "calumpeterwebb@icloud.com"}})
+}
+
+func CreateClient() *pluto.Client {
+	// TODO: add config file / environment file?
 	client := pluto.NewClient("test")
 
-	// Create Providers
+	// Create HTTP Provider
 	httpProvider := http.NewProvider("8080")
 
-	httpProvider.RegisterEndpoint(http.GetMethod, "/health-check", func() {
-
-	})
-
-	// TODO; think about how to add a middleware which could authenticate the user
-	// TODO; think about how to add the :userID
-	httpProvider.RegisterEndpoint(http.GetMethod, "/user/:userID", func() {
-
-	})
+	httpProvider.RegisterEndpoint(http.GetMethod, "/health-check", handleHealthCheck)
+	httpProvider.RegisterEndpoint(http.GetMethod, "/user/:id", handleGetUserById)
 
 	// Register the Providers
 	client.RegisterProvider(httpProvider)
 
+	return client
+}
+
+func RunClient() {
+	// Build the service
+	client := CreateClient()
+
 	// Start the service
 	client.Start()
+}
+
+func main() {
+	RunClient()
 }
--- services/users/main_test.go
+++ services/users/main_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type MainUnitTestSuite struct {
+	suite.Suite
+}
+
+func TestMainUnitTestSuite(t *testing.T) {
+	suite.Run(t, new(MainUnitTestSuite))
+}
+
+//func (s *MainUnitTestSuite) Test_HttpEndpoint_HealthCheck() {
+//	client := RunClient()
+//}
