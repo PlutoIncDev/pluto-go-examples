74b2b1a446d7a3b89c22aeae68ddbdab5e690ae0
Calum Webb <calumpeterwebb@icloud.com>
2021-06-07 15:06:57 -0700

    added skeleton for rpc

--- main.go
+++ main.go
@@ -4,24 +4,36 @@ import (
 	"log"
 	"pluto/pkgs/client"
 	"pluto/pkgs/providers/http"
+	"pluto/pkgs/providers/rpc"
+	"time"
 )
 
 func main() {
-	c := client.New("users")
+	/*
+
+	TODO: https://github.com/sirupsen/logrus
+
+	 */
+	var c *client.Client
+	c = client.New("users")
 
 	httpProvider := http.NewProvider("8080")
-
 	httpProvider.RegisterEndpoint("GET", "/health-check", func(ctx *http.Context) {
 		log.Println("GET /health-check")
 		ctx.HTTP.JSON(200, http.H{"success": "true"})
 	})
-
 	httpProvider.RegisterEndpoint("GET", "/users", func(ctx *http.Context) {
 		log.Println("GET /users")
 		ctx.HTTP.JSON(200, http.H{})
 	})
-
 	c.RegisterProvider(httpProvider)
 
-	c.Start(true)
+	rpcProvider := rpc.NewProvider()
+	// todo: add handlers?
+	c.RegisterProvider(rpcProvider)
+
+	c.Start(false)
+
+	time.Sleep(10 * time.Second)
+	c.Stop()
 }
